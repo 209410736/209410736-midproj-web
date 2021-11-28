fd020931e25197da9e7f9eb2ee2ddac64e467ecd
209410736 <209410736@gms.tku.edu.tw>
2021-11-28 22:05:34 +0800

    123

---
+++ src/pages/Midproj_36.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Midproj from '../components/midproj/Midproj';
+import Test from '../components/Test';
+
+
+const Midproj_36=()=> {
+    return (
+        <div >
+            <Midproj />
+            <Test/> 
+        </div>
+    );
+};
+
+export default Midproj_36
+
